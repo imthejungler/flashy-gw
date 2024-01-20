e91dfa6ba9c0a5652b1cb8bea579c65057800ba2
Julio C. Indriago <jcindriago@imthejungler.com>
2024-01-20 14:27:29 -0500

    #2: test: process payment response creation

--- checkout/service.py
+++ checkout/service.py
@@ -1,2 +1,13 @@
-def process_payment(amount) -> str:
-    return "APPROVED"
+import decimal
+
+import pydantic
+
+
+class PaymentResponse(pydantic.BaseModel):
+    amount: decimal.Decimal
+    amount: decimal.Decimal
+    status: str
+
+
+def process_payment(amount: decimal.Decimal) -> PaymentResponse:
+    return PaymentResponse(status="APPROVED", amount=amount)
--- test/checkout/test_a_payment.py
+++ test/checkout/test_a_payment.py
@@ -1,6 +1,7 @@
 from checkout import service as payment_service
+import decimal
 
 
 def test_should_approve_a_payment() -> None:
-    payment = payment_service.process_payment(amount=100)
-    assert payment == "APPROVED"
+    payment_response = payment_service.process_payment(amount=decimal.Decimal("100.00"))
+    assert payment_response.status == "APPROVED"
